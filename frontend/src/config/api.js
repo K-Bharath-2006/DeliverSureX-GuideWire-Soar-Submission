fdda7494e2d673e2d0393e326023782486243809
Bharath <gl.bharath2006@gmail.com>
2026-04-17 23:33:38 +0530

    url 2

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://localhost:5000/api';
+export const API_BASE_URL = 'https://deliversurex-backend-kfxe.onrender.com/api';
 
 const endpoints = {
   login: `${API_BASE_URL}/login`,
